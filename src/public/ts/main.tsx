15b489bdee0f770016ff876d8e25fa490eac9dd6
fChristenson <fredrik.christenson@gmail.com>
2020-04-12 10:51:32 +0200

    wp

--- src/public/ts/main.tsx
+++ src/public/ts/main.tsx
@@ -4,7 +4,16 @@ import { BrowserRouter, Route } from "react-router-dom";
 import { SearchPage } from "./SearchPage/SearchPage";
 import { SearchPage2 } from "./SearchPage/SearchPage2";
 
-// To help my bundling time at this level I can simply comment out the routes I'm not working on
+/**
+ * To help my bundling time at this level I can simply comment out the routes I'm not working on.
+ * However this will only work if I know that each node tree is isolated from the other node trees.
+ * If each of the node trees are coupled I will be slowed down by the code each tree shares.
+ *
+ * This makes modular architecture and only importing what you need essential.
+ * Make a habbit of having small files without shared logic.
+ * Shared logic can then be moved to shared files.
+ * This makes it easy to only include what is needed in the node tree.
+ */
 const App = () => (
   <BrowserRouter>
     <Route exact path="/" render={() => <h1>Hello World</h1>} />
